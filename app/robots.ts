6a9304462464aeec595f504f3cbfe28596f989f8
JanSzewczyk <jan.szewczyk1997@gmail.com>
2026-02-22 15:43:50 +0100

    fix: Ensure HTTPS protocol for Vercel URL in layout, robots, and sitemap files

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,11 @@ import { type MetadataRoute } from "next";
 import { env } from "~/data/env/client";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = env.NEXT_PUBLIC_VERCEL_URL || "https://localhost:3000";
+  const baseUrl = env.NEXT_PUBLIC_VERCEL_URL
+    ? env.NEXT_PUBLIC_VERCEL_URL.startsWith("http")
+      ? env.NEXT_PUBLIC_VERCEL_URL
+      : `https://${env.NEXT_PUBLIC_VERCEL_URL}`
+    : "https://localhost:3000";
 
   return {
     rules: [
