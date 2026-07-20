afed8eacfaebcbbfff2b6a9904684f659c4e6af5
JanSzewczyk <jan.szewczyk1997@gmail.com>
2026-07-20 14:58:20 +0200

    chore(cleanup): remove unused exports and types from portfolio page builder

--- tests/builders/index.ts
+++ tests/builders/index.ts
@@ -2,8 +2,3 @@ export {
   type ContactFormContent,
   contactFormContentBuilder
 } from "./contact-form-content.builder";
-export {
-  portfolioPageAboutBuilder,
-  portfolioPageBuilder,
-  portfolioPageHeroBuilder
-} from "./portfolio-page.builder";
